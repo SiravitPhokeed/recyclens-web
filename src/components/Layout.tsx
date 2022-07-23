58df543ac01a8b3f782a64184ce41e111bdb3e9a
Siravit Phokeed <siravitsasa@gmail.com>
2022-07-23 20:14:48 +0700

    feat: page transitions

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,6 +19,7 @@ import Link from "@components/Link";
 
 // Types
 import { RecycLensPage } from "@utils/types/common";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Layout = ({
   appBar,
@@ -30,76 +31,92 @@ const Layout = ({
   const router = useRouter();
 
   return (
-    <div className="pb-14">
-      <AppBar position="fixed">
-        <Toolbar>
-          {appBar?.backGoesTo ? (
-            <IconButton
+    <AnimatePresence
+      exitBeforeEnter
+      initial={false}
+      onExitComplete={() => window.scrollTo(0, 0)}
+    >
+      <div className="pb-14">
+        {/* App bar */}
+        <AppBar position="fixed">
+          <Toolbar>
+            {appBar?.backGoesTo ? (
+              <IconButton
+                LinkComponent={Link}
+                href={appBar.backGoesTo}
+                size="large"
+                edge="start"
+                color="inherit"
+                sx={{ mr: 2 }}
+              >
+                <MaterialSymbol icon="arrow_back" />
+              </IconButton>
+            ) : (
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                sx={{ mr: 2 }}
+              >
+                <MaterialSymbol icon="menu" />
+              </IconButton>
+            )}
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {appBar?.title || "RecycLens"}
+            </Typography>
+            {router.asPath != "/scan" && (
+              <IconButton
+                LinkComponent={Link}
+                href="/scan"
+                size="large"
+                edge="start"
+                color="inherit"
+              >
+                <MaterialSymbol icon="photo_camera" />
+              </IconButton>
+            )}
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+
+        {/* Content */}
+        <motion.div
+          key={router.asPath}
+          initial={{ y: -10 }}
+          animate={{ y: 0 }}
+          exit={{ y: -10 }}
+        >
+          {children}
+        </motion.div>
+
+        {/* Navigation */}
+        <Paper
+          sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+          elevation={3}
+        >
+          <BottomNavigation showLabels value={router.pathname}>
+            <BottomNavigationAction
+              label="Home"
+              icon={<MaterialSymbol icon="home" />}
               LinkComponent={Link}
-              href={appBar.backGoesTo}
-              size="large"
-              edge="start"
-              color="inherit"
-              sx={{ mr: 2 }}
-            >
-              <MaterialSymbol icon="arrow_back" />
-            </IconButton>
-          ) : (
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              sx={{ mr: 2 }}
-            >
-              <MaterialSymbol icon="menu" />
-            </IconButton>
-          )}
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {appBar?.title || "RecycLens"}
-          </Typography>
-          {router.asPath != "/scan" && (
-            <IconButton
+              href="/"
+            />
+            <BottomNavigationAction
+              label="Scan"
+              icon={<MaterialSymbol icon="photo_camera" />}
               LinkComponent={Link}
               href="/scan"
-              size="large"
-              edge="start"
-              color="inherit"
-            >
-              <MaterialSymbol icon="photo_camera" />
-            </IconButton>
-          )}
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-
-      {children}
-      
-      <Paper
-        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-        elevation={3}
-      >
-        <BottomNavigation showLabels value={router.pathname}>
-          <BottomNavigationAction
-            label="Home"
-            icon={<MaterialSymbol icon="home" />}
-            LinkComponent={Link}
-            href="/"
-          />
-          <BottomNavigationAction
-            label="Scan"
-            icon={<MaterialSymbol icon="photo_camera" />}
-            LinkComponent={Link}
-            href="/scan"
-          />
-          <BottomNavigationAction
-            label="Search"
-            icon={<MaterialSymbol icon="search" />}
-            LinkComponent={Link}
-            href="/local-guides"
-          />
-        </BottomNavigation>
-      </Paper>
-    </div>
+            />
+            <BottomNavigationAction
+              label="Search"
+              icon={<MaterialSymbol icon="search" />}
+              LinkComponent={Link}
+              href="/local-guides"
+            />
+          </BottomNavigation>
+        </Paper>
+      </div>
+    </AnimatePresence>
   );
 };
 
