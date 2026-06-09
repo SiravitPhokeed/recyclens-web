0ee79acddf4bbcd99155adfea9b9808eadc92dce
Siravit Phokeed <siravitsasa@gmail.com>
2026-06-09 22:11:57 +0700

    feat: add Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ import GlobalStyles from "@mui/material/GlobalStyles";
 import { emotionCache } from "@utils/emotion";
 import getDesignTokens from "@utils/theme";
 import { RecycLensPage } from "@utils/types/common";
+import { Analytics } from "@vercel/analytics/next";
 import { MotionConfig } from "motion/react";
 import type { AppProps } from "next/app";
 import { Grandstander, Rubik, Sarabun } from "next/font/google";
@@ -49,6 +50,7 @@ function App({
           </ThemeProvider>
         </AppCacheProvider>
       </MotionConfig>
+      <Analytics />
       <style>{`
         :root {
           --font-body: -apple-system, BlinkMacSystemFont,
